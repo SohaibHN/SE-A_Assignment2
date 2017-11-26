1e50ca51f0f5accaaaed73ef89d1a9f515eed0fa
sohaib.niwaz <sohaib.niwaz@gmail.com>
2017-11-26 13:15:29 +0530

    Inital Version - Database added and connected to application. Basic gui

---
+++ SE-A_Assignment2/database/dbo.Tickets.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tickets] (
+    [Id]          INT  NOT NULL,
+    [User]        TEXT NULL,
+    [Description] TEXT NULL,
+    [Project]     TEXT NULL,
+    [Status]      TEXT NULL,
+    [Severity]    TEXT NULL,
+    [Date Logged] DATE NULL,
+    [Deadline]    DATE NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
